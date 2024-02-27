001716cff08cc756dc22a2439d7aee2aa4dc557c
Andrea Silva S <andreasilva23433@gmail.com>
2024-02-27 08:53:41 -0500

    Correcion conexion

--- andrea/usuario.php
+++ andrea/usuario.php
@@ -9,7 +9,10 @@
     {
       $id_usuario= $_POST['id_usuario'];
       $nombre= $_POST['nombre'];
+	  $id_tipo_cargo= $_POST['id_tipo_cargo'];
+	  $id_estado= $_POST['id_estado'];
       $correo= $_POST['correo'];
+	  $id_tipo_usuario= $_POST['id_tipo_usuario'];
       $contrasena= $_POST['contrasena'];
       $nit_empresa= $_POST['nit_empresa'];
 
@@ -19,7 +22,7 @@
       
     
     
-      if($id_usuario=="" || $nombre=="" || $correo=="" || $contrasena=="" || $nit_empresa=="")
+      if($id_usuario=="" || $nombre=="" || $id_tipo_cargo=="" || $id_estado=="" || $correo=="" || $contrasena=="" || $nit_empresa=="")
       {
         echo '<script>alert ("EXISTEN DATOS VACIOS"); </script>';
         echo '<script>window.location="usuarios.php"</script>';
@@ -33,8 +36,8 @@
       else
       {
         $pass_cifrado=password_hash($contrasena,PASSWORD_DEFAULT,array("pass"=>12));
-        $insertSQL = $con->prepare ("INSERT INTO usuario(id_usuario,nombre, correo,contrasena,nit_empresa) 
-        VALUES ('$id_usuario','$nombre', '$correo', '$contrasena','$nit_empresa')");
+        $insertSQL = $con->prepare ("INSERT INTO usuario(id_usuario,nombre,id_tipo_cargo,id_estado,correo,id_tipo_usuario,contrasena,nit_empresa) 
+        VALUES ('$id_usuario','$nombre', '$id_tipo_cargo', '$id_estado', '$correo', '$id_tipo_usuario', '$contrasena','$nit_empresa')");
         $insertSQL->execute();
         echo '<script>alert ("registro exitoso"); </script>';
         echo '<script>window.location="usuarios.php"</script>';
@@ -96,6 +99,30 @@
 						</span>
 					</div>
 
+					<div class="wrap-input100 validate-input">
+					<select class="input100" name="id_tipo_cargo">
+							<?php
+							$control = $con->prepare("SELECT * FROM tipo_cargo");
+							$control->execute();
+							while ($fila = $control->fetch(PDO::FETCH_ASSOC)) {
+								echo "<option value='" . $fila['id_tipo_cargo'] . "'>" . $fila['cargo'] . "</option>";
+							}
+							?>
+						</select>
+					</div>
+
+					<div class="wrap-input100 validate-input">
+					<select class="input100" name="id_estado">
+							<?php
+							$control = $con->prepare("SELECT * FROM estado where id_estado <= 5");
+							$control->execute();
+							while ($fila = $control->fetch(PDO::FETCH_ASSOC)) {
+								echo "<option value='" . $fila['id_estado'] . "'>" . $fila['estado'] . "</option>";
+							}
+							?>
+						</select>
+					</div>
+
 
 					
 
@@ -105,6 +132,18 @@
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input">
+					<select class="input100" name="id_tipo_usuario">
+							<?php
+							$control = $con->prepare("SELECT * FROM tipos_usuarios");
+							$control->execute();
+							while ($fila = $control->fetch(PDO::FETCH_ASSOC)) {
+								echo "<option value='" . $fila['id_tipo_usuario'] . "'>" . $fila['tipo_usuario'] . "</option>";
+							}
+							?>
+						</select>
 					</div>
 
 
