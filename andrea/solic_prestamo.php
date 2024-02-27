e779be0454ed68f1d2d13b619ab6a964faeb3432
Andrea Silva S <andreasilva23433@gmail.com>
2024-02-27 07:29:12 -0500

    Conexion a la base de datos solic_prestamo.php

--- andrea/solic_prestamo.php
+++ andrea/solic_prestamo.php
@@ -4,6 +4,43 @@
     $db = new Database();
     $con =$db->conectar();
 ?>
+<?php
+    if ((isset($_POST["MM_insert"]))&&($_POST["MM_insert"]=="formreg"))
+    {
+      $id_prestamo= $_POST['id_prestamo'];
+      $id_usuario= $_POST['id_usuario'];
+      $monto_solicitado= $_POST['monto_solicitado'];
+      $valor_cuotas= $_POST['valor_cuotas'];
+	  $cant_cuotas= $_POST['cant_cuotas'];
+
+      $sql = $con -> prepare ("SELECT * FROM solic_prestamo where id_prestamo='$id_prestamo'");
+      $sql -> execute();
+      $fila = $sql -> fetchAll(PDO::FETCH_ASSOC);
+      
+    
+    
+      if($id_usuario=="" || $monto_solicitado=="" || $valor_cuotas=="" || $cant_cuotas=="")
+      {
+        echo '<script>alert ("EXISTEN DATOS VACIOS"); </script>';
+        echo '<script>window.location="usuarios.php"</script>';
+      }
+      else if($fila){
+        echo '<script>alert ("USUARIO O TELEFONO YA REGISTRADO"); </script>';
+        echo '<script>window.location="usuarios.php"</script>';
+      }
+
+            
+      else
+      {
+        $insertSQL = $con->prepare ("INSERT INTO solic_prestamo(id_prestamo,id_usuario, monto_solicitado,valor_cuotas,cant_cuotas) 
+        VALUES ('$id_prestamo','$id_usuario', '$monto_solicitado', '$valor_cuotas','$cant_cuotas')");
+        $insertSQL->execute();
+        echo '<script>alert ("registro exitoso"); </script>';
+        echo '<script>window.location="usuarios.php"</script>';
+      }
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,14 +73,13 @@
 					<img src="images/img-01.png" alt="IMG">
 				</div>
 
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" method="post">
 					<span class="login100-form-title">
 						Solicitud prestamo
 					</span>
-                    <form class="col-4 p-3" method="post">
 
 					<div class="wrap-input100">
-						<input class="input100" type="text" name="id_prestamo" id="id_prestamo" placeholder="id_prestamo" readonly>
+						<input class="input100" type="number" name="id_prestamo" id="id_prestamo" placeholder="id_prestamo" readonly>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -66,13 +102,6 @@
 						</span>
 					</div>
 
-                    <div class="wrap-input100 validate-input" data-validate = "Ingrese Estado">
-						<input class="input100" type="text" name="id_estado" id="id_estado" placeholder="Estado">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
-						</span>
-					</div>
 
                     <div class="wrap-input100 validate-input" data-validate = "Ingrese Valor Cuotas">
 						<input class="input100" type="text" name="valor_cuotas" id="valor_cuotas" placeholder="Valor Cuotas">
@@ -91,9 +120,8 @@
 					</div>
 					
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Registrar
-						</button>
+					<input class="login100-form-btn" type="submit" name="validar" value="Registrar">
+					<input type="hidden" name="MM_insert" value="formreg">
 					</div>
 
 					<div class="text-center p-t-12">
